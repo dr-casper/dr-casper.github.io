9195c4d92b0304382c028ea34e4004ff2932a27c
Nikita Smolenskii <nikita.smolenskii@gmail.com>
2018-09-07 18:07:54 +0200

    reduce boilerplate

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@
 
     // :: 1.0 Owl Carousel Active Code
     // :: 2.0 Slick Active Code
-    // :: 3.0 Footer Reveal Active Code
     // :: 4.0 ScrollUp Active Code
-    // :: 5.0 CounterUp Active Code
     // :: 6.0 onePageNav Active Code
     // :: 7.0 Magnific-popup Video Active Code
     // :: 8.0 Sticky Active Code
@@ -22,26 +20,6 @@
             smartSpeed: 1500,
             nav: true,
             navText: ["<i class='pe-7s-angle-left'</i>", "<i class='pe-7s-angle-right'</i>"]
-        });
-        $(".app_screenshots_slides").owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: true,
-            smartSpeed: 800,
-            margin: 30,
-            center: true,
-            dots: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                480: {
-                    items: 3
-                },
-                992: {
-                    items: 5
-                }
-            }
         });
     }
 
@@ -61,7 +39,6 @@
             speed: 500,
             asNavFor: '.slider-for',
             dots: true,
-            centerMode: true,
             focusOnSelect: true,
             slide: 'div',
             autoplay: true,
@@ -73,28 +50,11 @@
         });
     }
 
-    // :: 3.0 Footer Reveal Active Code
-    if ($.fn.footerReveal) {
-        $('footer').footerReveal({
-            shadow: true,
-            shadowOpacity: 0.3,
-            zIndex: -101
-        });
-    }
-
     // :: 4.0 ScrollUp Active Code
     if ($.fn.scrollUp) {
         $.scrollUp({
             scrollSpeed: 1500,
             scrollText: '<i class="fa fa-angle-up"></i>'
-        });
-    }
-
-    // :: 5.0 CounterUp Active Code
-    if ($.fn.counterUp) {
-        $('.counter').counterUp({
-            delay: 10,
-            time: 2000
         });
     }
 
@@ -125,10 +85,6 @@
 
     var $window = $(window);
 
-    if ($window.width() > 767) {
-        new WOW().init();
-    }
-
     // :: 8.0 Sticky Active Code
     $window.on('scroll', function () {
         if ($window.scrollTop() > 48) {
@@ -136,13 +92,6 @@
         } else {
             $('.header_area').removeClass('sticky slideInDown');
         }
-    });
-
-    // :: 9.0 Preloader Active code
-    $window.on('load', function () {
-        $('#preloader').fadeOut('slow', function () {
-            $(this).remove();
-        });
     });
 
 })(jQuery);
